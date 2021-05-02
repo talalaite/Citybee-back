b7e4327d2219795b536ab24594828286172d2f0a
talalaite <justina.talalaite@gmail.com>
2021-05-02 23:56:07 +0300

    /vehicles GET POST

--- src/index.js
+++ src/index.js
@@ -75,13 +75,67 @@ app.get("/modelscount", async (req, res) => {
   try {
     const con = await mysql.createConnection(mysqlConfig);
     const [data] = await con.execute(`
-    SELECT name, COUNT(vehicles.model_id) AS count, hourprice
+    SELECT name, 
+    COUNT(vehicles.model_id) AS count, 
+    hourprice
     FROM models
     INNER JOIN vehicles ON vehicles.model_id = models.id
     GROUP BY models.id
     `);
 
     return res.send(data);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ error: "Unexpected error has ocurred. Please try again later" });
+  }
+});
+
+// GET, POST /vehicles
+app.get("/vehicles", async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const [data] = await con.execute(`
+      SELECT models.name, (models.hourprice + models.hourprice * 0.21) AS hour_price_pvm, number_plate, country_location
+      FROM vehicles
+      INNER JOIN models ON (models.id = vehicles.model_id)
+      `);
+
+    return res.send(data);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ error: "Unexpected error has ocurred. Please try again later" });
+  }
+});
+
+app.post("/vehicles", async (req, res) => {
+  if (
+    !req.body.model_id ||
+    !req.body.number_plate ||
+    !req.body.country_location
+  ) {
+    return res.status(400).send({ error: "Incorrect data has been passed" });
+  }
+
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const [result] = await con.execute(
+      `INSERT INTO vehicles (model_id, number_plate, country_location) VALUES 
+      (${mysql.escape(req.body.model_id)}, 
+      ${mysql.escape(req.body.number_plate)}, 
+      ${mysql.escape(req.body.country_location)})`
+    );
+
+    if (!result.insertId) {
+      return res
+        .status(500)
+        .send({ error: "Execution failed. Please contact admin" });
+    }
+
+    return res.send({ id: result.insertId });
   } catch (err) {
     console.log(err);
     return res
